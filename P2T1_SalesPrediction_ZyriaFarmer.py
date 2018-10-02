3796ff95c57d49f4d59376151832af3e9a3fdada
ZyriaF <43758311+ZyriaF@users.noreply.github.com>
2018-10-02 14:46:25 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ZyriaFarmer.py
@@ -0,0 +1,13 @@
+#
+#10/2/18
+#CTI-110 P2T1-Sales Prediction
+#Zyria Farmer
+#
+ #Get the projected total sales
+total_sales = float(input("Enter the projected sales:"))
+
+#Calculate the profit as 23 percent of total sales
+profit = total_sales * 0.23
+
+#Display the profit
+print("The profit is $", profit)
